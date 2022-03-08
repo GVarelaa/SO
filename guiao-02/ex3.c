153e72e15a04c47d357ef2436e98a1e3e1bce065
GVarelaa <guilherme.varelaaa@gmail.com>
2022-03-08 15:45:31 +0000

    ex3 done

--- guiao-02/ex3.c
+++ guiao-02/ex3.c
@@ -4,5 +4,21 @@
 
 int main(int argc, char* argv[]){
 
+    for(int i = 1; i<=10; i++){
+        pid_t pid = fork();
+
+        if(pid == 0){
+            printf("Processo pai : %d\nProcesso filho : %d\n", getppid(), getpid());
+        
+            _exit(i);
+        }
+        else{
+            int status;
+            wait(&status);
+
+            printf("Codigo saida : %d\n", WEXITSTATUS(status));
+        }
+    }
+
     return 0;
 }
